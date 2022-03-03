a9f1bf5e22098ef9967db08ce4cbe5915d283174
Andres Liu <andres.liu22@gmail.com>
2022-03-02 20:41:39 -0800

    Add ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe("Recipe 1", "This is a description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"),
-    new Recipe("Recipe 1", "This is a description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
+    new Recipe("Recipe 2", "This is not a description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
   ];
 
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
--- src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping-list/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list-edit',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list-edit.component.css']
 })
 export class ShoppingListEditComponent implements OnInit {
-
+  @ViewChild('nameInput') name: ElementRef;
+  @ViewChild('amountInput') amount: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>()
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  
+  onAddIngredient() {
+    let newIngredient = new Ingredient(this.name.nativeElement.value, this.amount.nativeElement.value);
+    this.ingredientAdded.emit(newIngredient);
+    this.name.nativeElement.value = "";
+    this.amount.nativeElement.value = "";
+  }
 }
