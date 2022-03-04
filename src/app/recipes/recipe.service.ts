751f6e912264b4a174f3961f950f7d40904d97c1
Andres Liu <andres.liu22@gmail.com>
2022-03-03 20:22:11 -0800

    Add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,19 @@
 import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe("Recipe 1", "This is a description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272"),
-        new Recipe("Recipe 2", "This is not a description", "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272")
+        new Recipe("Hamburger", 
+        "This is a description", 
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272", 
+        [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
+        new Recipe("Double Burger", 
+        "This is not a description", 
+        "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272",
+        [new Ingredient('Buns', 2), new Ingredient('Meat', 2)])
     ];
 
     getRecipes() {
