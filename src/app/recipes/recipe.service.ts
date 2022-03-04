ef4f463e25330ed38b00c947ea2fa890d84e1160
Andres Liu <andres.liu22@gmail.com>
2022-03-03 20:28:58 -0800

    Add recipe ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter, Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
@@ -15,8 +16,16 @@ export class RecipeService {
         "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272",
         [new Ingredient('Buns', 2), new Ingredient('Meat', 2)])
     ];
+    
+    constructor(private shoppingListService: ShoppingListService){}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        ingredients.forEach(ingredient => {
+            this.shoppingListService.addIngredient(ingredient);
+        })
     }
 }
