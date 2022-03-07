5709f0dbf8642627dab4ec09b62a1e569427e6af
Andres Liu <andres.liu22@gmail.com>
2022-03-06 19:28:42 -0800

    Add routing

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -26,4 +26,8 @@ export class RecipeService {
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService.addIngredients(ingredients);
     }
+
+    getRecipe(id: number) {
+        return this.recipes.slice()[id];
+    }
 }
